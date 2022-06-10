92f7117a349218241cb8d98f528605f7f15abad7
chkegb <kegbsuc@gmail.com>
2022-06-10 14:54:38 +0200

    first first commit objet

--- src/entites/Personne.java
+++ src/entites/Personne.java
@@ -7,17 +7,32 @@ public class Personne {
 	AdressePostale adressePersonnelle;
 
 	// constructeur par defaut 
+	public Personne() {
+		
+	}
+	// constructeur avec des parametre
 	public Personne(String nom,String prénom,AdressePostale adressePersonnelle) {
 		this.nom=nom;
 		this.prénom=prénom;
 		this.adressePersonnelle =adressePersonnelle;
 			
 	}
-	public String getnom(){
+	//les getteurs
+	public String getNom(){
 		return this.nom;
 	}
 	public String getPrenom(){
 		return this.prénom;
 	}
-	
-    }
+	public AdressePostale getAdressePersonnelle() {
+		
+		return this.adressePersonnelle;
+	}
+	//les accessuers
+	public void setNom(string nom) {
+		this.nom=nom;
+	}
+	public void setPreNom(string prenom) {
+		this.prénom=prenom;
+	}
+ }
