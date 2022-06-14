432690a5b8a78b7dd372a4163ab72237fa995da1
chkegb <kegbsuc@gmail.com>
2022-06-14 11:34:16 +0200

    wind appr

--- src/entites/TestPersonne.java
+++ src/entites/TestPersonne.java
@@ -6,15 +6,25 @@ public class TestPersonne {
 		// TODO Auto-generated method stub
 
 		//Créer 2 instances de la classe Personne en renseignant la valeur de tous les attributs.
-		Personne p1=new Personne();
-		Personne p2=new Personne();
-		p1.nom ="Jacques";
+		Personne p1=new Personne("Jacques","Laurent");
+		
+		//Personne p2=new Personne();
+		
+		/*
+		 * p1.nom ="Jacques";
+		 
 		p1.prénom ="Laurent";
 		// les affectations de l'instances de personne liées à la classe adresseP
 		p1.adressePersonnelle.code_postal=34970 ;
 		p1.adressePersonnelle.libellé_de_la_rue="test";
 		p1.adressePersonnelle.numéro_de_rue=25;
 		p1.adressePersonnelle.ville ="Lattes";
+		
+		*/
+		
+		System.out.println(p1.nom);
+		System.out.println(p1.prénom);
+		System.out.println(p1.adressePersonnelle);
 	}
 
 }
